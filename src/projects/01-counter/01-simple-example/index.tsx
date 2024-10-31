8ce48450ba63c64383d24e06647617389ab41eff
Dmitry Dubinin <34936248+Dimetio@users.noreply.github.com>
2024-10-31 17:40:17 +0300

    feat: добавлен обычный вариант счетчика

---
+++ src/projects/01-counter/01-simple-example/index.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Result, Button, Header, ContentWrapper } from "../shared/components";
+
+/** Вариант с использованием `useState` */
+function SimpleCounter() {
+  const [counter, setCounter] = useState<number>(0);
+
+  function handleDecrease() {
+    setCounter((counter) => counter - 1);
+  }
+
+  function handleIncrease() {
+    setCounter((counter) => counter + 1);
+  }
+
+  function handleReset() {
+    setCounter(0);
+  }
+
+  return (
+    <ContentWrapper>
+      <Header title="Simple Counter" />
+
+      <Result counter={counter} />
+
+      <div className="grid gap-3 sm:grid-cols-3">
+        <Button
+          className="bg-red-500 hover:bg-red-400"
+          onClick={handleDecrease}
+        >
+          -
+        </Button>
+
+        <Button
+          className="bg-neutral-500 hover:bg-neutral-400"
+          onClick={handleReset}
+        >
+          Reset
+        </Button>
+
+        <Button
+          className="bg-green-500 hover:bg-green-400"
+          onClick={handleIncrease}
+        >
+          +
+        </Button>
+      </div>
+    </ContentWrapper>
+  );
+}
+
+export default SimpleCounter;
