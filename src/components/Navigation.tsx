89092560a1c7fcecafff69a22637fb9de061c538
Dmitry Dubinin <34936248+Dimetio@users.noreply.github.com>
2024-12-05 16:54:58 +0300

    feat: Добавил роутинг

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,34 @@
+import { Link, NavLink } from "react-router-dom";
+
+import clsx from "clsx";
+
+const routes = [
+  { path: "/counter", title: "Counter" },
+  { path: "stopwatch", title: "StopWatch" },
+];
+
+function Navigation() {
+  return (
+    <>
+      <Link to="/">Главная</Link>
+
+      <nav className="space-x-2">
+        {routes.map(({ path, title }) => (
+          <NavLink to={path}>
+            {({ isActive }) => (
+              <span
+                className={clsx("hover:border-b hover:border-violet-500 hover:text-violet-500", {
+                  "font-semibold text-violet-700": isActive,
+                })}
+              >
+                {title}
+              </span>
+            )}
+          </NavLink>
+        ))}
+      </nav>
+    </>
+  );
+}
+
+export default Navigation;
