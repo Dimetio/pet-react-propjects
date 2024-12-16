5ca7c34a9b37274d1caaf7e6209cceed3b609874
Dmitry Dubinin <34936248+Dimetio@users.noreply.github.com>
2024-12-16 14:44:03 +0300

    feat: добавил toastify в проект

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router";
+import { ToastContainer } from "react-toastify";
 
 import App from "./App.tsx";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
       <App />
+
+      <ToastContainer />
     </HashRouter>
   </StrictMode>,
 );
